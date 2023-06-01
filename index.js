c179709aedbd7650d24a81e6a07b227d1932495a
devZahid <officialzahidhasan19932021@gmail.com>
2023-06-01 13:59:13 +0600

    hiding db user and password

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-
+require('dotenv').config()
 //middleware
 app.use(cors());
 app.use(express.json());
@@ -14,7 +14,8 @@ app.use(express.json());
 
 
 
-const uri = "mongodb+srv://zahidhasan:uKVoNBzc3ZcCmQj0@cluster0.uoombu0.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER_NAME}:${process.env.DB_USER_PASSWORD}@cluster0.uoombu0.mongodb.net/?retryWrites=true&w=majority`;
+
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
