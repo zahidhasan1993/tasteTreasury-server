ee315bccefd2706173ad94a7e2e5a110a7dd3a92
devZahid <officialzahidhasan19932021@gmail.com>
2023-06-07 15:03:20 +0600

    adding jwt

--- index.js
+++ index.js
@@ -1,21 +1,39 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
+const jwt = require('jsonwebtoken');
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-require('dotenv').config()
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+
 //middleware
 app.use(cors());
 app.use(express.json());
 
+const varifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
 
+  // console.log("jwt function autho : ",authorization);
+  if (!authorization) {
+    return res.status(401).send({ error: true, message: "unauthorize user" });
+  }
+  const token = authorization.split(" ")[1];
+  // console.log("token from jwt function :", token);
+  // verify a token symmetric
+  jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
+    if (err) {
+      return res
+        .status(401)
+        .send({ error: true, message: "unauthorized user" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
 
-
-
-
+//mongoDb connections
 const uri = `mongodb+srv://${process.env.DB_USER_NAME}:${process.env.DB_USER_PASSWORD}@cluster0.uoombu0.mongodb.net/?retryWrites=true&w=majority`;
-
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -23,7 +41,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -31,67 +49,102 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     //db database collection
-    const database = client.db('TasteTreasury');
-    const menuCollection = database.collection('menu');
-    const reviewsCollection = database.collection('reviews');
-    const cartCollection = database.collection('cart');
-    const userCollection = database.collection('user');
-
+    const database = client.db("TasteTreasury");
+    const menuCollection = database.collection("menu");
+    const reviewsCollection = database.collection("reviews");
+    const cartCollection = database.collection("cart");
+    const userCollection = database.collection("user");
+    //jwt token
+    app.post("/jwt", (req, res) => {
+      const body = req.body;
+      const token = jwt.sign(body, process.env.ACCESS_TOKEN, {
+        expiresIn: '1h',
+      });
+      res.send({ token });
+    });
     //server connections
-    app.get('/menu', async(req,res) => {
+    app.get("/menu", async (req, res) => {
       const result = await menuCollection.find().toArray();
       res.send(result);
-    })
-    app.get('/review', async(req,res) => {
+    });
+    app.get("/review", async (req, res) => {
       const result = await reviewsCollection.find().toArray();
       res.send(result);
-    })
-    app.get('/cart/:email', async (req,res) => {
-      const data =  req.params.email;
-      const query = { email : data };
+    });
+    app.get("/cart/:email", varifyJWT, async (req, res) => {
+      const email = req.params.email;
+      const decodedEmail = req.decoded.email;
+      if (!email) {
+        res.send([]);
+      }
+      if (email !== decodedEmail) {
+        return res
+          .status(403)
+          .send({ error: true, message: "Not your directory" });
+      }
+      const query = { email: email };
       
-      // console.log(data);
 
       const result = await cartCollection.find(query).toArray();
       res.send(result);
-    })
-    app.get('/users', async (req,res) => {
+    });
+    app.get("/users", async (req, res) => {
       const result = await userCollection.find().toArray();
       res.send(result);
-    })
+    });
+    // make admin api / update admin role
+    app.patch("/users/admin/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: "admin",
+        },
+      };
+      const result = await userCollection.updateOne(filter, updateDoc);
+
+      res.send(result);
+    });
     // send data to db
-    app.post('/cart', async (req,res) => {
+    app.post("/cart", async (req, res) => {
       const item = req.body;
       const result = await cartCollection.insertOne(item);
 
       res.send(result);
-
-    })
-    app.post('/users', async (req,res) => {
+    });
+    app.post("/users", async (req, res) => {
       const user = req.body;
-      const query = {email: user.email};
-      
+      const query = { email: user.email };
+
       const exitingUser = await userCollection.findOne(query);
       // console.log(exitingUser);
       if (exitingUser) {
-        return res.send('user all ready exists')
+        return res.send("user all ready exists");
       }
-      
+
       const result = await userCollection.insertOne(user);
-      res.send(result)
-    })
+      res.send(result);
+    });
     //delete data from DB
-    app.delete('/cart/:id', async (req,res) => {
+    app.delete("/cart/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await cartCollection.deleteOne(query);
 
-      res.send(result)
+      res.send(result);
+    });
+    app.delete("/user/delete/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await userCollection.deleteOne(query);
 
-    })
+      res.send(result);
+    });
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -99,16 +152,12 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-
 //connection
 
-app.get('/', (req,res) => {
-    res.send('Welcome to TasteTreasury Server');
-})
+app.get("/", (req, res) => {
+  res.send("Welcome to TasteTreasury Server");
+});
 
 app.listen(port, () => {
-    console.log('running on port', port);
-})
-
-
+  console.log("running on port", port);
+});
